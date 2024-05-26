187edff11de8c72be487f7353f71d065f30f9b88
Dimitris Zervas <dzervas@dzervas.gr>
2024-05-27 00:45:31 +0300

    Implement serde for face & ring
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- src/decompose/ring.rs
+++ src/decompose/ring.rs
@@ -1,8 +1,10 @@
+use serde::{Deserialize, Serialize};
+
 use crate::primitives::circle::Circle;
 
 use super::segment::Segment;
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub enum Ring {
     Circle(Circle),
     Segments(Vec<Segment>),
