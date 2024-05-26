2a8fb834d6ea2cdd7cd750befc221a1053906be5
Tobias Jacob <tobi.jacob@gmx.net>
2024-05-26 21:45:19 +0200

    Desc

--- src/decompose/mod.rs
+++ src/decompose/mod.rs
@@ -1,4 +1,4 @@
-use crate::sketch::Sketch;
+use crate::{primitives::Primitive, sketch::Sketch};
 
 use self::face::Face;
 
@@ -7,9 +7,16 @@ pub mod ring;
 pub mod segment;
 
 pub fn decompose_sketch(sketch: &Sketch) -> Vec<Face> {
-    for primitive in sketch.primitives() {
-        let primitive = primitive.borrow().to_primitive();
-    }
+    let primitives = sketch
+        .primitives()
+        .iter()
+        .map(|p| p.borrow().to_primitive())
+        .collect::<Vec<Primitive>>();
+    // A primitive is now ether a Circle, Line, or Arc. Points can be ignored.
+    // Now chain all consecutive primitives that are connected into a ring.
+    // - Two primitives are connected if the end of the first primitive is the start of the second primitive.
+    // For now, assume there is only one ring in the sketch, such that the construction of the faces is simple.
+    // For the future, we will need a more complex algorithm that can handle multiple rings. But for the MVP, this is sufficient.
 
     todo!("Decompose the sketch into faces")
 }
