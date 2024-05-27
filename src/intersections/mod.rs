2ddc6922509cadc306f50a970ad52b70e02af16b
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-05-27 14:59:41 +1000

    remove the trait because it will be annoying to have line intersect point and point intersect line duplicated

--- src/intersections/mod.rs
+++ src/intersections/mod.rs
@@ -1,73 +1,61 @@
 use approx::relative_eq;
-use nalgebra::Vector2;
 
 use crate::primitives::{arc::Arc, circle::Circle, line::Line, point2::Point2, Primitive};
 
 const INTERSECT_EPS: f64 = 1e-6;
 
-pub fn intersections(_a: Primitive, _b: Primitive) -> Vec<Vector2<f64>> {
+pub fn intersections(a: Primitive, b: Primitive) -> Vec<nalgebra::Point2<f64>> {
     // We only deal with point intersections for now
     // In case two lines do overlap in parallel, we just treat the whole sketch as invalid
     // In case of two arcs or circles with same radius in same position, we treat the whole sketch as invalid, etc.
-    todo!("Find all intersections between two primitives")
+    match (a, b) {
+        (Primitive::Line(l), Primitive::Point2(p)) => line_intersect_point(&l, &p),
+        _ => todo!("Find all intersections between two primitives"),
+    }
 }
 
-pub(crate) trait Intersect2D {
-    fn intersect_point(&self, other: &Point2) -> Option<Vec<nalgebra::Point2<f64>>>;
-    fn intersect_line(&self, other: &Line) -> Option<Vec<nalgebra::Point2<f64>>>;
-    fn intersect_arc(&self, other: &Arc) -> Option<Vec<nalgebra::Point2<f64>>>;
-    fn intersect_circle(&self, other: &Circle) -> Option<Vec<nalgebra::Point2<f64>>>;
+fn line_intersect_point(line: &Line, p: &Point2) -> Vec<nalgebra::Point2<f64>> {
+    // Based on https://stackoverflow.com/a/328110
+    let line_end = line.end_pt();
+    let line_start = line.start_pt();
+    let line_vector = line_end - line_start;
+    let point: nalgebra::Point2<f64> = p.data().into();
+    let point_vector = line_end - point;
+
+    // p lies somewhere on the line if all three points are collinear.
+    // check that cross product of End-Start and End-Other is 0
+    let collinear = relative_eq!(
+        line_vector.cross(&point_vector).magnitude(),
+        0.0,
+        epsilon = INTERSECT_EPS
+    );
+    if !collinear {
+        return vec![]; // Early exit
+    }
+
+    // check that P lies between Start and End by comparing either it's x or y.
+    let within_segment = if line_start.x != line_end.x {
+        within_range(p.x(), line_start.x, line_end.x, INTERSECT_EPS)
+    } else {
+        within_range(p.y(), line_start.y, line_end.y, INTERSECT_EPS)
+    };
+
+    if !within_segment {
+        vec![]
+    } else {
+        vec![point]
+    }
 }
 
-impl Intersect2D for Line {
-    /// Check that point `p` lies on the line segment defined by this [Line]
-    fn intersect_point(&self, p: &Point2) -> Option<Vec<nalgebra::Point2<f64>>> {
-        // Based on https://stackoverflow.com/a/328110
-        let line_end = self.end_pt();
-        let line_start = self.start_pt();
-        let line_vector = line_end - line_start;
-        let point: nalgebra::Point2<f64> = p.data().into();
-        let point_vector = line_end - point;
+fn line_intersect_line(line_a: &Line, line_b: &Line) -> Vec<nalgebra::Point2<f64>> {
+    todo!()
+}
 
-        // p lies somewhere on the line if all three points are collinear.
-        // check that cross product of End-Start and End-Other is 0
-        let collinear = relative_eq!(
-            line_vector.cross(&point_vector).magnitude(),
-            0.0,
-            epsilon = INTERSECT_EPS
-        );
-        if !collinear {
-            return None; // Early exit
-        }
-
-        // check that P lies between Start and End by comparing either it's x or y.
-        let within_segment = if line_start.x != line_end.x {
-            within_range(p.x(), line_start.x, line_end.x, INTERSECT_EPS)
-        } else {
-            within_range(p.y(), line_start.y, line_end.y, INTERSECT_EPS)
-        };
-
-        if !within_segment {
-            None
-        } else {
-            Some(vec![point])
-        }
-    }
-
-    fn intersect_line(&self, other: &Line) -> Option<Vec<nalgebra::Point2<f64>>> {
-        // easy
-        None
-    }
-
-    fn intersect_arc(&self, other: &Arc) -> Option<Vec<nalgebra::Point2<f64>>> {
-        // Up to two intersections
-        None
-    }
-
-    fn intersect_circle(&self, other: &Circle) -> Option<Vec<nalgebra::Point2<f64>>> {
-        // Up to two intersections
-        None
-    }
+fn line_intersect_circle(line: &Line, circle: &Circle) -> Vec<nalgebra::Point2<f64>> {
+    todo!()
+}
+fn line_intersect_arc(line: &Line, arc: &Arc) -> Vec<nalgebra::Point2<f64>> {
+    todo!()
 }
 
 #[inline]
